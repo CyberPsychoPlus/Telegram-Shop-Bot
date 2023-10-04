dd5cee9afc22b9b5d590c002e09615744e1f1ddc
Nick <142791566+CyberPsychoPlus@users.noreply.github.com>
2023-10-04 15:47:08 +0300

    v.1.0
    First version of bot

---
+++ TelegramBot.py
@@ -0,0 +1,61 @@
+import asyncio
+from aiogram import Bot, Dispatcher, F
+from aiogram.types import Message, CallbackQuery
+
+# Імпорт власної бібліотеки
+import keyboards as kb
+
+bot = Bot(token='6226072857:AAHCvXbN8iuyeh9TQ80RVR8ZPFuK-hJlyKI')
+dp = Dispatcher()
+
+
+# Початок праці з ботом
+@dp.message(F.text == '/start')
+async def cmd_start(message: Message):
+    # Будь який інший напис замість(Вітаємо!)
+    await message.answer('Вітаємо!', reply_markup=kb.main)
+
+
+# Обробник контактів
+@dp.message(F.text == 'Контакти')
+async def contacts(message: Message):
+    await message.answer('Наші контакти: ', reply_markup=kb.socials)
+
+
+# Обробник товарів
+@dp.message(F.text == 'Товари')
+async def catalog(message: Message):
+    await message.answer('Наші товари: ', reply_markup=kb.catalog)
+
+
+@dp.callback_query(F.data == 'Bitcoin')
+async def cb_bitcoin(callback: CallbackQuery):
+    await callback.answer(f'Ви обрали {callback.data}', show_alert=True)
+    await callback.message.answer(f'Ви обрали {callback.data}')
+
+
+# Обробник інформації
+@dp.message(F.text == 'Інформація')
+async def inform(message: Message):
+    await message.answer('Інформація про бота...'
+                         'Тут може бути що завгодно', reply_markup=kb.inform)
+
+
+# @dp.message() Відповідає користувачеві на будь-які дії, крім команд.
+# Повинен використовуватися завжди в кінці, інакше інші не спрацюють
+@dp.message()
+async def echo(message: Message):
+    await message.answer('Будьласька, оберіть команду')
+
+
+# Нескінченна робота бота
+async def main():
+    await dp.start_polling(bot)
+
+# Дефолт фукція
+if __name__ == '__main__':
+    # Обробка вийняків
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print('Бот завершил работу.')
---
+++ keyboards.py
@@ -0,0 +1,42 @@
+from aiogram.types import (ReplyKeyboardMarkup, KeyboardButton,
+                           InlineKeyboardMarkup, InlineKeyboardButton)
+
+
+# Створення кнопок
+main_kb = [
+    [KeyboardButton(text='Інформація'),
+     KeyboardButton(text='Товари')],
+    [KeyboardButton(text='Контакти')]
+]
+
+# Створення клавіатури
+main = ReplyKeyboardMarkup(keyboard=main_kb,
+                           resize_keyboard=True,
+                           input_field_placeholder='Оберіть пункт нижче')
+
+
+# Створення контактів
+socials = InlineKeyboardMarkup(inline_keyboard=[
+    # Будь яке ваше посилання
+    [InlineKeyboardButton(
+        text='Telgram', url='https://www.google.com.ua/')],
+    [InlineKeyboardButton(
+        text='YouTube', url='https://www.google.com.ua/')]
+])
+
+
+# Створення каталогу товарів
+catalog = InlineKeyboardMarkup(inline_keyboard=[
+    # Товари такж для приклау і можуть бути замінені
+    [InlineKeyboardButton(text='Bitcoin', callback_data='Bitcoin')],
+    [InlineKeyboardButton(text='Ethereum', callback_data='Ethereum')],
+    [InlineKeyboardButton(text='Dogecoin', callback_data='Dogecoin')]
+])
+
+
+# Інформація про бота
+inform = InlineKeyboardMarkup(inline_keyboard=[
+    # Будь яка інформація про бота
+    [InlineKeyboardButton(
+        text='Наш сайт', url='https://uk.wikipedia.org/wiki/')]
+])
